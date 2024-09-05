4715576949fb3ff9577078498e5b89b236c3458e
NicoSwag <nicololg30@gmail.com>
2024-09-05 15:01:30 +1000

    rival fix in granite

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -675,7 +675,7 @@
 #define FLAG_GENERATOR_ACTIVE  0x278 // Unused Flag
 #define FLAG_RECEIVED_GRANITE_ESCAPE_ROPE  0x279 // Unused Flag
 #define FLAG_BRINEY_MENTIONED_BACKSTORY  0x27A // Unused Flag
-#define FLAG_UNUSED_0x27B  0x27B // Unused Flag
+#define FLAG_HIDE_GRANITE_RIVAL  0x27B // Unused Flag
 #define FLAG_UNUSED_0x27C  0x27C // Unused Flag
 #define FLAG_UNUSED_0x27D  0x27D // Unused Flag
 #define FLAG_UNUSED_0x27E  0x27E // Unused Flag
