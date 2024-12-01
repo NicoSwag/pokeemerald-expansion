234f6f68d2e1e9107c10a4516ce9c46c9ec4e5f3
nico <140973355+NicoSwag@users.noreply.github.com>
2024-12-02 09:18:47 +1100

    polish

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -186,7 +186,7 @@ $(OBJEVENTGFXDIR)/people/matt.4bpp: %.4bpp: %.png
 $(OBJEVENTGFXDIR)/people/engineer.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
-$(OBJEVENTGFXDIR)/people/umbrella_lady.4bpp: %.4bpp: %.png
+$(OBJEVENTGFXDIR)/people/parasol_lady.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
 $(OBJEVENTGFXDIR)/people/miner.4bpp: %.4bpp: %.png
